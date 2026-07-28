d8a087d7c9810c2c014ee0b794031a9302da0ec8
Lautaro Rodriguez <lautygamer2023@gmail.com>
2026-07-28 18:29:23 -0300

    JKGFDFTY

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const supabaseUrl = 'https://mvmilbpraefwprexgnpz.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12bWlsYnByYWVmd3ByZXhnbnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjA1MzcsImV4cCI6MjA4ODUzNjUzN30.xH72_trpTpJhtZJw0BXI-Sewp9vnbBigKhmVBNI4wso';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-
+//
 const PAYPAL_CLIENT_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID;
 
 const PLATFORMS = [
